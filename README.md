3e3d34ef7844aa249c114a045cbe70cfb775c433
lyashenkogs <lyashenkogs@gmail.com>
2017-12-23 00:15:54 +0200

    Simplest implementation

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # TechAssignment
-Here may be some technical assignment to write an algorithm
+
+## Solution:
+* Create a temporary file
+* iterate over lines over the original file
+* for each line iterate over characters and copy all elements to a new String
+checking if the character not belong to a comment block
+* append the new String to the temporary file
+
+Suggested implementation iterates over all characters in a file so the algorithm 
+time complexity is O(n) where n is number of characters in the original file
+
+Space complexity. To perform algorithm it requires to load in memory a single line and create 
+a new line. This is try for files of any size, so required RAM size is constant but the size
+of a temporary file linearly depends on the original file size so the space complexity
+is O(n) 
+ 
+A more efficient and complex solution may be implemented using  java.io.RandomAccessFile 
+with an in-place algorithm
+
+
--- src/test/java/com/lyashenkogs/AppTest.java
+++ src/test/java/com/lyashenkogs/AppTest.java
@@ -2,11 +2,78 @@ package com.lyashenkogs;
 
 import org.junit.Test;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
+
+import static org.junit.Assert.assertEquals;
+
 
 public class AppTest {
 
-    @Test
-    public void test() {
 
+    private static String removeComments(String input) {
+        System.out.println("input String:   " + input);
+        char[] chars = input.toCharArray();
+        StringBuilder outputBuilder = new StringBuilder();
+        for (int i = 0; i < chars.length; i++) {
+            //check if an element is a comment sign
+            if (chars[i] == '/' && chars[i + 1] == '*') {
+                //iterate until closed bracket
+                for (int j = i + 2; j < chars.length; j++) {
+                    if (chars[j] == '*' && chars[j + 1] == '/') {
+                        //start new iteration from the and of a comment block
+                        i = j + 1;
+                        break;
+                    }
+                }
+            } else {
+                outputBuilder.append(chars[i]);
+            }
+        }
+        System.out.println("cleaned String: " + outputBuilder.toString());
+        return outputBuilder.toString();
     }
+
+    private static void cleanComments(File output, File input) throws IOException {
+        Files.deleteIfExists(output.toPath());
+        Scanner scanner = new Scanner(input);
+        BufferedWriter writer = new BufferedWriter(new FileWriter(output, true));
+        while (scanner.hasNext()) {
+            //for each line remove comments and append to a new file
+            String nextLine = scanner.nextLine();
+            String cleaned = removeComments(nextLine);
+            writeLine(writer, cleaned);
+        }
+    }
+
+    private static void writeLine(BufferedWriter writer, String string) throws IOException {
+        writer.append(string);
+        writer.newLine();
+        writer.flush();
+    }
+
+    @Test
+    public void removeCommentsFromFile() throws IOException {
+        cleanComments(new File("Output.txt"), new File("Input.txt"));
+        List<String> exampleLines = Files.readAllLines(Paths.get("ExampleOutput.txt"));
+        List<String> outputLines = Files.readAllLines(Paths.get("Output.txt"));
+        assertEquals(exampleLines, outputLines);
+    }
+
+    @Test
+    public void removeCommentsFromLine() {
+        String cleaned = removeComments("This is /*not */so cool.");
+        assertEquals("This is so cool.", cleaned);
+    }
+
+
 }
+
+
+
