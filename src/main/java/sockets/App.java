7476ae7a057ce6c6d2e1f4699f0b5603e61f7291
lyashenkogs <lyashenkogs@gmail.com>
2018-03-31 23:16:31 +0300

    Simplest working solution for GET

--- src/main/java/sockets/App.java
+++ src/main/java/sockets/App.java
@@ -3,6 +3,7 @@ package sockets;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -11,48 +12,46 @@ public class App {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         //parse request URI using a raw socket
-        new Thread(() -> {
-            ServerSocket serverSocket = null;
-            try {
-                serverSocket = new ServerSocket(8080);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        final boolean[] isFinish = {false};
+        Thread mainThread = new Thread(() -> {
+            String line = null;
+            BufferedReader bufferedReader = null;
             Socket accept = null;
             try {
+                ServerSocket serverSocket = null;
+                serverSocket = new ServerSocket(8080);
                 accept = serverSocket.accept();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            BufferedReader bufferedReader = null;
-            try {
                 bufferedReader = new BufferedReader(new InputStreamReader(accept.getInputStream()));
-            } catch (IOException e) {
+                line = bufferedReader.readLine();
+            } catch (Exception e) {
                 e.printStackTrace();
             }
-            while (true) {
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                String line = null;
+            while (line != null && !line.equals("")) {
+                System.out.println(line);
                 try {
                     line = bufferedReader.readLine();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-                while (line != null) {
-                    System.out.println(line);
-                    try {
-                        line = bufferedReader.readLine();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
             }
-        }).start();
+            //send response
+            try {
+                OutputStream outputStream = accept.getOutputStream();
+                //               outputStream.write("HTTP/1.1 200 OK\n".getBytes());
+                outputStream.write("some asdfasfd".getBytes());
+                //    outputStream.write("Connection:keep-alive\n".getBytes());
+                //    outputStream.write("Content-Type:text/html\".getBytes());
+                outputStream.flush();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
+        });
+        mainThread.start();
+      /*  while (!isFinish[0]) {
+            System.out.println("await finishing");
+            Thread.sleep(100);
+        }*/
     }
 
 }
