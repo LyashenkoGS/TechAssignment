46220d7050f6927fddc9c785ab8763e780aa8a4e
lyashenkogs <lyashenkogs@gmail.com>
2018-04-08 01:17:23 +0300

    Implement the HTTP server to return a file by given name in URI

--- src/main/java/sockets/App.java
+++ src/main/java/sockets/App.java
@@ -1,28 +1,37 @@
 package sockets;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import fi.iki.elonen.NanoHTTPD;
+
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Paths;
 
 public class App {
 
+    private static final String DELIMITER = "\r\n";
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-        //parse request URI using a raw socket
-        final boolean[] isFinish = {false};
+    public static void main(String[] args) {
+        //process incoming HTTP request in a new thread
         Thread mainThread = new Thread(() -> {
             String line = null;
             BufferedReader bufferedReader = null;
             Socket accept = null;
+            String fileName = "";
+            ServerSocket serverSocket = null;
             try {
-                ServerSocket serverSocket = null;
                 serverSocket = new ServerSocket(8080);
+                serverSocket.setReuseAddress(true);
                 accept = serverSocket.accept();
+                System.out.println("START CUSTOM SERVER");
                 bufferedReader = new BufferedReader(new InputStreamReader(accept.getInputStream()));
                 line = bufferedReader.readLine();
+                //parse URI
+                String[] tokens = line.split(" ");
+                fileName = tokens[1].replace("/", "");
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -36,22 +45,68 @@ public class App {
             }
             //send response
             try {
+                File file = new File(fileName);
+                byte[] responseBody;
+                if (file.canRead()) {
+                    responseBody = Files.readAllBytes(file.toPath());
+                } else {
+                    responseBody = ("can't read the file by name: " + fileName).getBytes();
+                }
                 OutputStream outputStream = accept.getOutputStream();
-                //               outputStream.write("HTTP/1.1 200 OK\n".getBytes());
-                outputStream.write("some asdfasfd".getBytes());
-                //    outputStream.write("Connection:keep-alive\n".getBytes());
-                //    outputStream.write("Content-Type:text/html\".getBytes());
+                outputStream.write(("HTTP/1.1 200 OK" + DELIMITER).getBytes());
+                outputStream.write(("Content-Type: text/html" + DELIMITER).getBytes());
+                outputStream.write(("Content-Length: " + responseBody.length + DELIMITER).getBytes());
+                outputStream.write(DELIMITER.getBytes());
+                outputStream.write(responseBody);
                 outputStream.flush();
+                System.out.println("CUSTOM SERVER RESPONSE HAS BEEN SEND");
             } catch (Exception e) {
                 e.printStackTrace();
             }
+            try {
+                serverSocket.close();
+                accept.close();
+                System.out.println("CUSTOM SERVER:SOCKET IS CLOSED");
 
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         });
         mainThread.start();
-      /*  while (!isFinish[0]) {
-            System.out.println("await finishing");
-            Thread.sleep(100);
-        }*/
+    }
+
+    static class NanoHTTTPDexample extends NanoHTTPD {
+
+        static NanoHTTTPDexample instance;
+
+        public NanoHTTTPDexample() throws IOException {
+            super(8080);
+            start(NanoHTTPD.SOCKET_READ_TIMEOUT, false);
+            System.out.println("\nRunning! Point your browsers to http://localhost:8080/ \n");
+        }
+
+        public static void main() {
+            try {
+                instance = new NanoHTTTPDexample();
+            } catch (IOException ioe) {
+                System.err.println("Couldn't start server:\n" + ioe);
+            }
+        }
+
+        @Override
+        public NanoHTTPD.Response serve(IHTTPSession session) {
+            String fileName = session.getUri().replace("/", "");
+            byte[] bytes = new byte[0];
+            try {
+                bytes = Files.readAllBytes(Paths.get(fileName));
+            } catch (NoSuchFileException e) {
+                return newFixedLengthResponse("can't read the file by name: " + fileName);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println("CUSTOM SERVER RESPONSE HAS BEEN SEND");
+            return newFixedLengthResponse(new String(bytes, Charset.defaultCharset()));
+        }
     }
 
 }
--- src/test/java/sockets/AppTest.java
+++ src/test/java/sockets/AppTest.java
@@ -1,21 +1,16 @@
 package sockets;
 
-import org.junit.Ignore;
+import org.junit.After;
 import org.junit.Test;
 
-import javax.xml.bind.DatatypeConverter;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertTrue;
@@ -23,48 +18,21 @@ import static org.junit.Assert.assertTrue;
 
 public class AppTest {
 
-    @Test
-    @Ignore
-    //TODO finish implementing a web server on a raw socket
-    public void readRequestAsBytes() throws Exception {
-        // App.main(new String[]{});
-        final ServerSocket[] serverSocket = new ServerSocket[1];
-        final Socket[] accept = new Socket[1];
-        Thread thread = new Thread(() -> {
-            try {
-                serverSocket[0] = new ServerSocket(8000);
-                accept[0] = serverSocket[0].accept();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-        thread.start();
+    @After
+    public void tearDown() throws Exception {
+        //prevents an error due to unclosed socket
         Thread.sleep(2_000);
-        //given a socket
-        URL yahoo = new URL("http://127.0.0.1:8000/test");
-        URLConnection yc = yahoo.openConnection();
-        yc.setDoOutput(true);
-        yc.setDoInput(true);
-        OutputStream outputStream = yc.getOutputStream();
-        //then send a request
-        byte[] hexBinary = DatatypeConverter.parseHexBinary("0fb8");
-        outputStream.write(hexBinary);
-        outputStream.flush();
-        outputStream.close();
-        Thread.sleep(1_000);
-        //so we will get a request as a byte array
-        InputStream inputStream = accept[0].getInputStream();
-        byte[] response = readBytes(inputStream);
-        System.out.println(Arrays.toString(response));
     }
 
     @Test
     public void getSomeFile() throws Exception {
         //given a web server
         App.main(new String[]{});
+        Thread.sleep(1_000);
         //then sent an http request
         URL yahoo = new URL("http://127.0.0.1:8080/someFile.html");
         URLConnection yc = yahoo.openConnection();
+        System.out.println(yc.getHeaderFields());
         InputStream inputStream = yc.getInputStream();
         byte[] response = readBytes(inputStream);
         System.out.println("actual:\n" + new String(response));
@@ -75,9 +43,45 @@ public class AppTest {
     }
 
     @Test
+    public void nanoHTTPDgetFile1() throws IOException, InterruptedException {
+        //given a web server
+        App.NanoHTTTPDexample.main();
+        Thread.sleep(1_000);
+        //then sent an http request
+        URL yahoo = new URL("http://127.0.0.1:8080/someFile.html");
+        URLConnection yc = yahoo.openConnection();
+        System.out.println(yc.getHeaderFields());
+        InputStream inputStream = yc.getInputStream();
+        byte[] response = readBytes(inputStream);
+        System.out.println("actual:\n" + new String(response));
+        //so we will get a file in the response
+        byte[] someHTML1Bytes = Files.readAllBytes(Paths.get("someFile.html"));
+        System.out.println("expected:\n" + new String(someHTML1Bytes, Charset.defaultCharset()));
+        assertArrayEquals(someHTML1Bytes, response);
+        App.NanoHTTTPDexample.instance.closeAllConnections();
+    }
+
+    @Test
+    public void nanoHTTPDgetUnexistedFile() throws Exception {
+        //given a web server
+        App.NanoHTTTPDexample.main();
+        Thread.sleep(1_000);
+        //then sent an http request to a wrong URI
+        URL yahoo = new URL("http://127.0.0.1:8080/aaaaa.html");
+        URLConnection yc = yahoo.openConnection();
+        InputStream inputStream = yc.getInputStream();
+        byte[] response = readBytes(inputStream);
+        //so we will get an error message
+        System.out.println(new String(response));
+        assertTrue(new String(response).contains("can't read the file by name: " + "aaaaa.html"));
+        App.NanoHTTTPDexample.instance.closeAllConnections();
+    }
+
+    @Test
     public void getSomeFile2() throws Exception {
         //given a web server
         App.main(new String[]{});
+        Thread.sleep(1_000);
         //then sent an http request
         URL yahoo = new URL("http://127.0.0.1:8080/someFile2.html");
         URLConnection yc = yahoo.openConnection();
@@ -94,13 +98,15 @@ public class AppTest {
     public void getUnexistedFile() throws Exception {
         //given a web server
         App.main(new String[]{});
+        Thread.sleep(1_000);
         //then sent an http request to a wrong URI
         URL yahoo = new URL("http://127.0.0.1:8080/aaaaa.html");
         URLConnection yc = yahoo.openConnection();
         InputStream inputStream = yc.getInputStream();
         byte[] response = readBytes(inputStream);
         //so we will get an error message
-        assertTrue(new String(response).contains("unexpected URI: " + "/aaaaa.html"));
+        System.out.println(new String(response));
+        assertTrue(new String(response).contains("can't read the file by name: " + "aaaaa.html"));
     }
 
     private byte[] readBytes(InputStream inputStream) throws IOException {
@@ -113,7 +119,6 @@ public class AppTest {
         buffer.flush();
         return buffer.toByteArray();
     }
-
 
 }
 
